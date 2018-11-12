b6d386472b6c7cc650d254b3901282db3dc6db11
zcnet4 <zcnet4@gmail.com>
2018-11-12 17:11:14 +0800

    [M]update protobuf-2.6.1 android lib

--- protobuf-2.6.1/android/jni/Application.mk
+++ protobuf-2.6.1/android/jni/Application.mk
@@ -1,6 +1,6 @@
-APP_PLATFORM := android-14
+APP_PLATFORM := android-19
 APP_MODULES := protobuf
-APP_STL := gnustl_static
+APP_STL := gnustl_shared
 APP_ABI := armeabi-v7a
 APP_CPPFLAGS += -frtti -std=c++11 -fexceptions -pthread
 NDK_TOOLCHAIN_VERSION := 4.9
