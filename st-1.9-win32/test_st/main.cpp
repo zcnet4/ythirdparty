d00a21673cad0a99af08b244f80584158d11b2a2
zcnet4 <zcnet4@gmail.com>
2018-12-05 14:32:12 +0800

    [M]add st-1.9-win32

---
+++ st-1.9-win32/test_st/main.cpp
@@ -0,0 +1,46 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include "st.h"
+#include <stdio.h>
+#include <errno.h>
+
+static st_thread_t maintid;
+static int count = 0;
+
+void *mythread(void *arg)
+{
+  st_thread_t *t1 = (st_thread_t*)arg;
+
+  printf("thread %d started. main=%d\n", st_thread_self(), t1);
+  st_sleep(1);
+  st_thread_interrupt(maintid);
+  printf("thread %d finished\n", st_thread_self());
+  count++;
+  return(0);
+}
+
+int main(int argc, char *argv[])
+{
+  st_thread_t tid;
+  int rc;
+
+  st_init();
+  rc = EINTR;
+  maintid = st_thread_self();
+  printf("main tid=%d\n", maintid);
+  while (rc == EINTR)
+  {
+    if (count == 10)
+      break;
+    tid = st_thread_create(mythread, maintid, 1, 0);
+    printf("tid %d created\n", tid);
+    rc = st_sleep(-1);
+    if (rc == -1) rc = errno;
+  }
+  printf("main finished. rc=%d\n", rc);
+  return(0);
+}
+
